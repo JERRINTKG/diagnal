f554dddccf516ed5309f78f5a8022f87ae4c638c
jerrintkg <jerin.t@twinsway.in>
2020-03-21 15:51:21 +0530

    stor update.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { createStore,combineReducers } from "redux";
 import { movies } from "reducers/movies";
+import { music } from "reducers/music";
 
 const rootReducer = combineReducers({
-  movies
+  movies,
+  music
 });
 
 export default createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
