f9112ef41ae3708801aef3fb566b9053f682edac
jerrintkg <jerin.t@twinsway.in>
2020-03-22 21:27:39 +0530

    Updates

---
+++ precache-manifest.d09a3cc9e5b26670273eaf30e8b3cb1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a66cf0798d3a7f96497876fe3ee0832",
+    "url": "/diagnal/index.html"
+  },
+  {
+    "revision": "453718fb304527d1af99",
+    "url": "/diagnal/static/css/main.119390ab.chunk.css"
+  },
+  {
+    "revision": "9d14983aef643ef66ac2",
+    "url": "/diagnal/static/js/2.d6e69f83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/diagnal/static/js/2.d6e69f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453718fb304527d1af99",
+    "url": "/diagnal/static/js/main.d83d5914.chunk.js"
+  },
+  {
+    "revision": "b7d18aebb6f5d24860ef",
+    "url": "/diagnal/static/js/runtime-main.456d1b2c.js"
+  }
+]);
