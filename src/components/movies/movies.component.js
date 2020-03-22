633779a898b0d461eadfddf93b594a1c676f5a63
jerrintkg <jerin.t@twinsway.in>
2020-03-22 20:05:38 +0530

    updated.

--- src/components/movies/movies.component.js
+++ src/components/movies/movies.component.js
@@ -5,9 +5,9 @@ const MovieContainer = (props) => {
     
   return (
     <React.Fragment>
-      <div className="h-40 w-24 text-gray-700 text-center bg-gray-400 m-2">
+      <div className="h-40 w-24 text-gray-700 bg-gray-400 m-2">
         <div className="h-40 w-24" style={{backgroundImage: `url(slices/${props.movie['poster-image']})` }} />
-        <div>{props.movie.name}</div>
+        <div className="py-1 text-sm">{props.movie.name}</div>
       </div>
     </React.Fragment>
   );
